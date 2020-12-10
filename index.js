95b079f3b31b6c4bf7f5aeb5061489ead82ef3d6
Muhidin <muhidinhukic1@gmail.com>
2020-12-09 20:23:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+document.addEventListener("keydown", function(count) {
+    if (count.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+
+let dodger = document.getElementById('dodger');
+
+
+function moveDodgerLeft() {
+    let currentNumber = dodger.style.left.replace('px', "")
+    let moveNumber = parseInt(currentNumber)
+
+    if (moveNumber > 0) {
+        dodger.style.left = `${moveNumber - 10}px`
+    }
+}
+
+
+document.addEventListener("keydown", function(count) {
+    if(count.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
+function moveDodgerRight() {
+
+    let currentNumber = dodger.style.left.replace('px', '')
+    let moveNumber = parseInt(currentNumber)
+
+    if (moveNumber >=0 && moveNumber !== 360) {
+        dodger.style.left = `${moveNumber + 10}px`
+    }
+}
